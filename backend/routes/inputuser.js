c16fb349d5407f0336a9fbf80a7f6e3ca4e29ab0
Noah Giesel <noahgiesel@gmail.com>
2020-04-03 15:35:59 +0200

    CRUD backend completed, frontned needs fix on deleting db

--- backend/routes/inputuser.js
+++ backend/routes/inputuser.js
@@ -1,20 +1,36 @@
 const router = require("express").Router();
 let User = require("../models/user.input.post")
 
+let  DB_ARRAY = []
 
-router.route("/:id").get((req,res) => {
-    User.findById(req.params.id)
-        .then(input => res.json(input))
-        .catch(err => res.status(400).json("error: "+ err))
+ 
+router.route("/get").get((req,res) => {
+    User.find({}).lean().exec(function(error, records) {
+        records.forEach(function(record) { 
+           DB_ARRAY = [...DB_ARRAY, ( {
+            id : record._id,
+            value : record.inputvalue
+         }) ]
+        }) 
+        res.send(DB_ARRAY)
+    })
+    DB_ARRAY = []
 })
 
 
 router.route("/add").post((req,res) => {  
-    const newString = new User(req.body)  // dive the input to model 
+    const newString = new User(req.body)  // dive the input to model  
     newString.save() // save it to db
-    .then(() => res.json("Array added") ) // return sended feedback
+    .then(() => res.json("added: " + req.body.inputvalue)) // return sended feedback
     .catch(err => res.status(400).json("error: "+ err)) //Return error feedback
 })
 
+
+router.route("/deleteTitle").delete((req,res) => {
+    User.deleteMany().exec(); 
+    DB_ARRAY = []
+    res.send("deleted all database")
+})
+ 
 
 module.exports = router;
--- src/App.js
+++ src/App.js
@@ -7,18 +7,74 @@ function App() {
 
   const [elements , setElements] = useState([]);
   const [inputValue, setInputValue] = useState("")
+  const [counter, setCounter] = useState(0) 
+  const [elementsArray, setElementsArray] = useState([])
 
- 
+  const inputList = elements.map((e) => <li key={e.id}>{e.value}</li> );
+  const serverList = elementsArray.map((e) => <li key={e.id}>{e.value}</li> );
   
   const send = () =>  { 
+    setInputValue("")
+    setElements([...elements, {
+      id: counter,
+      value : inputValue
+    } ])
+    setCounter(counter+1) 
+    postInput()
+  }
+  
+  const retrive = () =>  {  
+     getInput()
+  }
 
-    console.log('Click happened');
+  
+  const EmptyDatabase = () =>  { 
+    emptyDatabase()
+    setElements("")
+    setInputValue("")
   }
 
 
+
+
+  const postInput = () => { 
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ inputvalue:inputValue })
+    };
+    fetch('http://localhost:5000/array/add', requestOptions)  
+     } 
+  const getInput = () => { 
+    const requestOptions = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify()
+    };
+    fetch('http://localhost:5000/array/get', requestOptions)
+        .then(response => response.json())  
+        .then(data => copyPassedArray(data)) 
+     } 
+
+     const copyPassedArray = data => { 
+       for(let i = 0 ; i< data.length; i++) { 
+         elementsArray[i] = data[i]
+       }
+       console.log(elementsArray)
+     }
+     
+  const emptyDatabase = () => { 
+    const requestOptions = {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify()
+    };
+    fetch('http://localhost:5000/array/deleteTitle', requestOptions)   
+     } 
+
   return (
     <div className="App">
-      <h1>Inserisci una parola da aggiungere... </h1> 
+      <h1 className="title">Inserisci una parola da aggiungere ! </h1> 
       <div className="box">
         <input 
           type="text" 
@@ -27,9 +83,27 @@ function App() {
           placeholder="Say something..." id="in" 
         />
       </div>
-        <button onClick={send}>Copy this!</button>  
+      <div className="button-grid">
+        <button onClick={send}>Send Input</button>  
+        <button onClick={retrive}>Retrieve Word</button>  
 
-        {inputValue}
+      </div>
+
+      <div className="container-grid">
+        <div className="input-given">
+          <h2>input informations </h2>
+          <ul>{inputList}</ul>
+        </div>
+        <div className="out-puts">
+          <h2>output informations </h2>
+
+        </div>
+        <div className="server-status">
+          <h2>server informations </h2>
+          <ul>{serverList}</ul>
+        </div>
+      </div>
+        <button className="buttonEmpty" onClick={EmptyDatabase}>Empty Database</button>  
     </div>
   );
 }
